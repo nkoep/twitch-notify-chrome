044229b719bc02434bf916dff1e20560ea85377d
Niklas Koep <niklas.koep@gmail.com>
2015-07-03 09:16:59 +0200

    Add `rc` directory for semantic-ui config files

--- build.sh
+++ build.sh
@@ -35,50 +35,17 @@ cd $semanticdir
 npm install --ignore-scripts
 
 # Configure semantic-ui
-cat > semantic.json <<EOF
-{
-  "base": "",
-  "paths": {
-    "source": {
-      "config"      : "src/theme.config",
-      "definitions" : "src/definitions/",
-      "site"        : "src/site/",
-      "themes"      : "src/themes/"
-    },
-    "output": {
-      "packaged"     : "dist/",
-      "uncompressed" : "dist/components/",
-      "compressed"   : "dist/components/",
-      "themes"       : "dist/themes/"
-    },
-    "clean"        : "dist/"
-  },
-  "permission" : false,
-  "rtl": false,
-  "components": [
-    "button",
-    "icon",
-    "input",
-    "form",
-    "table",
-    "checkbox"
-  ]
-}
-EOF
-
+cp "$basedir/rc/semantic.json" .
 move src/theme.config.example src/theme.config
 move src/_site src/site
 # Override device breakpoints to force desktop sizes
-cat > src/site/globals/site.variables <<EOF
-@largestMobileScreen: 0px;
-@largestTabletScreen: 0px;
-EOF
+cp "$basedir/rc/site.variables" src/site/globals/
 
 # Build semantic-ui
 gulp build
 
 # Install semantic-ui
-cd "$basedir"
+cd -
 cp "$builddir/semantic.min.js" js/
 cp "$builddir/semantic.min.css" css/
 mkdir -p css/themes/
