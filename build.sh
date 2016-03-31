6e237a2da71ff4c6cb1fd4f8dbb744b9c5025655
Niklas Koep <niklas.koep@gmail.com>
2016-03-31 08:48:19 +0200

    build.sh: install files in specified directory
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- build.sh
+++ build.sh
@@ -7,6 +7,11 @@ die() {
   exit 1
 }
 
+if [ $# -ne 1 ]; then
+  die "Output directory required"
+fi
+outdir="$1"
+
 # Check if npm, bower and gulp can be found in PATH.
 for p in npm bower gulp; do
   if [ ! -f $(which $p 2>/dev/null) ]; then
@@ -15,32 +20,37 @@ for p in npm bower gulp; do
 done
 
 # Directory definitions
-basedir=$(pwd)
+basedir="$(pwd)"
 bowerdir="$basedir/bower_components"
 semanticdir="$bowerdir/semantic"
 builddir="$semanticdir/dist"
 
 # Download semantic-ui
 bower install semantic
-cd $semanticdir
+cd "$semanticdir"
 npm install --ignore-scripts
 
 # Configure semantic-ui
 cp "$basedir/rc/semantic.json" .
 cp src/theme.config.example src/theme.config
 cp -R src/_site src/site
-cp "$basedir/rc/site.variables" src/site/globals/
+cp "$basedir/rc/site.variables" src/site/globals
 
 # Build semantic-ui
 gulp build
+cd "$basedir"
 
 # Install semantic-ui
-cd -
-cp "$builddir/semantic.min.js" js/
-cp "$builddir/semantic.min.css" css/
-mkdir -p css/themes/
-cp -R "$builddir/themes/default" css/themes/
+mkdir -p "$outdir"
+
+cp -R js css "$outdir"
+cp *.html *.json "$outdir"
+
+cp "$builddir/semantic.min.js" "$outdir/js"
+cp "$builddir/semantic.min.css" "$outdir/css"
+mkdir "$outdir/css/themes"
+cp -R "$builddir/themes/default" "$outdir/css/themes"
 
 # Install jquery (gets fetched as a dependency of semantic-ui anyway)
-cp "$bowerdir/jquery/dist/jquery.min.js" js/
+cp "$bowerdir/jquery/dist/jquery.min.js" "$outdir/js"
 
